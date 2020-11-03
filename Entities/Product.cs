917539c7a52d2ba3890d8b83bd4d81d955f0f683
Matheus de Assis <assis4002@gmail.com>
2020-11-03 14:08:18 -0300

    Completed exercise

---
+++ Entities/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Qts { get; set; }
+
+        public Product(string name, double price, int qts)
+        {
+            Name = name;
+            Price = price;
+            Qts = qts;
+        }
+
+        public double Total()
+        {
+            return Qts * Price;
+        }
+    }
+
+
+
+
+
+
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Entities;
+using System;
+using System.Globalization;
 using System.IO;
 
 namespace Exercise04_Files
@@ -9,25 +11,39 @@ namespace Exercise04_Files
         {
             Console.WriteLine("Enter file full path: ");
 
-            string sourceFolderPath = Console.ReadLine();
+            string sourceFilePath = Console.ReadLine();
 
             try
             {
-                string[] lines = File.ReadAllLines(sourceFolderPath);
+                string[] lines = File.ReadAllLines(sourceFilePath);
 
-                string sourceFolderPath = Path.GetDirectoryName(sourceFolderPath);
+                string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
                 string targetFolderPath = sourceFolderPath + @"\out";
                 string targetFilePath = targetFolderPath + @"\summary.csv";
 
-                Directory.CreateDirectory(targetFilePath);
+                Directory.CreateDirectory(targetFolderPath);
 
                 using (StreamWriter sw = File.AppendText(targetFilePath))
                 {
                     foreach(string line in lines)
                     {
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int qts = int.Parse(fields[2]);
+
+                        Product product = new Product(name, price, qts);
+
+                        sw.WriteLine(product.Name + ", " + product.Total().ToString("F2", CultureInfo.InvariantCulture));
 
                     }
                 }
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
             }
         }
     }
