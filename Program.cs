efd8bcc8413a3dc8f608c611f1d555204f5f23a9
Matheus de Assis <assis4002@gmail.com>
2020-11-03 13:27:26 -0300

    Project Started

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace Exercise04_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter file full path: ");
+
+            string sourceFolderPath = Console.ReadLine();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourceFolderPath);
+
+                string sourceFolderPath = Path.GetDirectoryName(sourceFolderPath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+
+                Directory.CreateDirectory(targetFilePath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach(string line in lines)
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
